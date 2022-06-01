50b437149ec577606f986333f500d18e343187ef
oluwoleadetifa <66252275+oluwoleadetifa@users.noreply.github.com>
2022-06-01 23:05:11 +0100

    swiping feature

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,5 +1,6 @@
-import React, { Children, cloneElement, useState } from 'react'
+import React, { Children, cloneElement, useEffect, useState } from 'react'
 import { images } from '../../assets/images';
+import { useSwipeable } from 'react-swipeable';
 import Image from '../Image/Image';
 import './Carousel.style.scss';
 
@@ -16,18 +17,42 @@ const Carousel = ({ children }) => {
   //transform position index state management
   const [position, setPosition] = useState(0);
 
+  //pause slide show state management
+  const [paused, setPaused] = useState(false);
+
   const updatePosition = (newPosition) => {
     if (newPosition < 0) {
-      newPosition = 0;
+       newPosition = Children.count(children) - 1;
     } else if (newPosition >= Children.count(children)) {
-      newPosition = Children.count(children) - 1;
+      newPosition = 0;
     }
 
     setPosition(newPosition)
   }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!paused) {
+        updatePosition(position + 1);
+      }
+    }, 3000);
+
+    return () => {
+      if (interval) {
+        clearInterval(interval)
+      }
+    }
+  })
+
+
+  //handler for swipping effect
+  const handlers = useSwipeable({
+    onSwipedLeft: () => updatePosition(position + 1),
+    onSwipedRight: () => updatePosition(position - 1),
+  });
   return (
     <>
-      <div className="carousel">
+      <div {...handlers} className="carousel" onMouseEnter={() => setPaused(true)} onMouseLeave={()=> setPaused(false)}>
         <div
           className="inner"
           style={{ transform: `translateX(-${position * 100}%)` }}

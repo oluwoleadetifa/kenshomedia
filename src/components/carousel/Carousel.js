fb2e075fc4a63bf4995db02620a601d3a24363bb
oluwoleadetifa <66252275+oluwoleadetifa@users.noreply.github.com>
2022-06-01 12:57:05 +0100

    components

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { Children, cloneElement, useState } from 'react'
+import './Carousel.style.scss';
 
-const Carousel = () => {
+export const CarouselItem = ({ children, width }) => {
+  //carousel item div
   return (
-    <div>Carousel</div>
-  )
+    <div className='carousel-item' style={{width: width}}>
+      {children}
+    </div>
+  );
+};
+
+const Carousel = ({ children }) => {
+  //transform position index state management
+  const [position, setPosition] = useState(0);
+
+  const updatePosition = (newPosition) => {
+    if (newPosition < 0) {
+      newPosition = 0;
+    } else if (newPosition >= Children.count(children)) {
+      newPosition = Children.count(children) - 1;
+    }
+
+    setPosition(newPosition)
+  }
+  return (
+    <>
+      <div className="carousel-1">
+        <div
+          className="inner"
+          style={{ transform: `translateX(-${position * 100}%)` }}
+        >
+          {Children.map(children, (child, index) => {
+            return cloneElement(child, { width: "100%" });
+          })}
+        </div>
+      </div>
+      <div className='indicators'>
+        <button onClick={() => updatePosition(position - 1)}>back</button>
+        <button onClick={() => updatePosition(position + 1)}>next</button>
+      </div>
+    </>
+  );
 }
 
 export default Carousel
--- src/containers/main.js
+++ src/containers/main.js
@@ -1,18 +1,39 @@
 import React from 'react'
 import { images } from '../assets/images'
+import Carousel, { CarouselItem } from '../components/carousel/Carousel'
 import Image from '../components/Image/Image'
+import '../components/main/Main.style.scss'
 
 const Main = () => {
   return (
-    <div className='main-body'>
-      <div className='carousel'>
-        <Image src={images.Advert}/>
+    <>
+      <div className="main-body">
+        <div className="carousel">
+          <Image src={images.Advert} className={"sliding-images"} />
+        </div>
+        <div className="ad-object">
+          <div className="card">
+            <div className="text">Slot of the week</div>
+            <Image src={images.Game} className="card-image" />
+          </div>
+        </div>
       </div>
-      <div className='ad-object'>
-
-      </div>
-    </div>
-  )
+      <Carousel>
+        <CarouselItem>
+          <Image src={images.Advert} className={"sliding-images"} />
+        </CarouselItem>
+        <CarouselItem>
+          <Image src={images.Advert} className={"sliding-images"} />
+        </CarouselItem>
+        <CarouselItem>
+          <Image src={images.Advert} className={"sliding-images"} />
+        </CarouselItem>
+        <CarouselItem>
+          <Image src={images.Advert} className={"sliding-images"} />
+        </CarouselItem>
+      </Carousel>
+    </>
+  );
 }
 
 export default Main
